3a0cf0f69de2a088b4dfc7251a5b50bfd911b67a
Anderson Velásquez <andersonjosvela@gmail.com>
2025-03-27 20:07:39 -0600

    Actualizacion del modelo de Productos, se agregaron esquemas para POST y PUT

--- Models/Product.cs
+++ Models/Product.cs
@@ -10,4 +10,22 @@ namespace simpleApiRestDesafioEy.Models
         [Range(1, double.MaxValue, ErrorMessage = "El precio debe ser mayor que 0")]
         public required decimal Price { get; set; }
     }
+
+    public class CreateProductDto
+    {
+        [Required(ErrorMessage = "El nombre del producto es obligatorio")]
+        public required string Name { get; set; }
+        [Required(ErrorMessage = "El precio es obligatorio...")]
+        [Range(1, double.MaxValue, ErrorMessage = "El precio debe ser mayor que 0")]
+        public required decimal Price { get; set; }
+    }
+
+    public class UpdateProductDto
+    {
+        [Required(ErrorMessage = "El nombre del producto es obligatorio")]
+        public required string Name { get; set; }
+        [Required(ErrorMessage = "El precio es obligatorio...")]
+        [Range(1, double.MaxValue, ErrorMessage = "El precio debe ser mayor que 0")]
+        public required decimal Price { get; set; }
+    }
 }
