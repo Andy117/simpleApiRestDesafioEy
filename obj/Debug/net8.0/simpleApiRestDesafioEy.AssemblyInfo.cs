c22e24c06ffb2947e978e90cfc2fe225cf4e4278
Anderson Velásquez <andersonjosvela@gmail.com>
2025-03-27 21:37:11 -0600

    Commit final a los archivos generados por .NET

--- obj/Debug/net8.0/simpleApiRestDesafioEy.AssemblyInfo.cs
+++ obj/Debug/net8.0/simpleApiRestDesafioEy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("simpleApiRestDesafioEy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f94f3ecec83f6ebd1287822b0de1921c8b1322f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("simpleApiRestDesafioEy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("simpleApiRestDesafioEy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
