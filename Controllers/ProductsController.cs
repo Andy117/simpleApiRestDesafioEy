a34deaa5810a49082324a49da8ca26ff2b8aa8db
Anderson Velásquez <andersonjosvela@gmail.com>
2025-03-27 19:06:20 -0600

    Modificacion del controlador para utilzar la instancia ProductService

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,77 +1,52 @@
 using Microsoft.AspNetCore.Mvc;
-using MyApi.Models;
+using simpleApiRestDesafioEy.Models;
+using simpleApiRestDesafioEy.Services;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace MyApi.Controllers
+namespace simpleApiRestDesafioEy.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private static readonly List<Product> Products = new List<Product>
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
         {
-            new Product {Id = 1, Name = "Nintendo Switch", Price = 3999},
-            new Product {Id = 2, Name = "Teclado", Price = 500}
-        };
+            _productService = productService;
+        }
 
         [HttpGet]
         public ActionResult<IEnumerable<Product>> GetProducts()
         {
-            return Ok(Products);
+            return Ok(_productService.GetProducts());
         }
 
         [HttpGet("{id}")]
         public ActionResult<Product> GetProduct(int id)
         {
-            var product = Products.FirstOrDefault(p => p.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            else
-            {
-                return Ok(product);
-            }
+            var product = _productService.GetProduct(id);
+            return product == null ? NotFound() : Ok(product);
         }
 
         [HttpPost]
         public ActionResult<Product> CreateProduct(Product product)
         {
-            product.Id = Products.Count + 1;
-            Products.Add(product);
-            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+            var createdProduct = _productService.CreateProduct(product);
+            return CreatedAtAction(nameof(GetProduct), new { id = createdProduct.Id }, createdProduct);
+
         }
 
         [HttpPut("{id}")]
-        public IActionResult UpdateProduct(int id, Product updatedProduct)
+        public IActionResult UpdateProduct(int id, Product product)
         {
-            var product = Products.FirstOrDefault(p => p.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            else
-            {
-                product.Name = updatedProduct.Name;
-                product.Price = updatedProduct.Price;
-                return NoContent();
-            }
+            return _productService.UpdateProduct(id, product) ? NoContent() : NotFound();
         }
 
         [HttpDelete("{id}")]
         public IActionResult DeleteProduct(int id)
         {
-            var product = Products.FirstOrDefault(p => p.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            else
-            {
-                Products.Remove(product);
-                return NoContent();
-            }
+            return _productService.DeleteProduct(id) ? NoContent() : NotFound();
         }
     }
 }
